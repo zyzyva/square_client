360fcca073865bf0fd459fbea7e076a2c2bbb4f9
zyzyva <instockscraper@gmail.com>
2025-10-08 20:04:06 -0400

    Use env base sdk url for front end form

--- lib/square_client.ex
+++ lib/square_client.ex
@@ -64,4 +64,42 @@ defmodule SquareClient do
   use the string directly: `"https://connect.squareup.com/v2"`
   """
   def production_api_url, do: "https://connect.squareup.com/v2"
+
+  @doc """
+  Returns the Square sandbox Web Payments SDK URL for development and testing.
+
+  Use this in your HTML layouts to load the correct SDK version:
+  `<script src="<%= SquareClient.sandbox_sdk_url() %>"></script>`
+  """
+  def sandbox_sdk_url, do: "https://sandbox.web.squarecdn.com/v1/square.js"
+
+  @doc """
+  Returns the Square production Web Payments SDK URL.
+
+  Use this in your HTML layouts to load the correct SDK version:
+  `<script src="<%= SquareClient.production_sdk_url() %>"></script>`
+  """
+  def production_sdk_url, do: "https://web.squarecdn.com/v1/square.js"
+
+  @doc """
+  Returns the appropriate Web Payments SDK URL based on the configured API URL.
+
+  This is the recommended way to load the SDK in your layouts as it automatically
+  selects the correct environment based on your :square_client configuration.
+
+  ## Example
+
+      # In your root.html.heex
+      <script type="text/javascript" src={SquareClient.sdk_url()}>
+      </script>
+  """
+  def sdk_url do
+    api_url = Application.get_env(:square_client, :api_url)
+
+    if String.contains?(api_url || "", "sandbox") do
+      sandbox_sdk_url()
+    else
+      production_sdk_url()
+    end
+  end
 end
